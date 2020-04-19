acde335b2abe550b265055dc00eac2f4c20b6495
ochiboy <chieshing@gmail.com>
2020-04-19 17:45:46 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-o1kle7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-o1kle7)
---
+++ src/app/products.ts
@@ -0,0 +1,34 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: ''
+  },
+  {
+    name: 'Samsung S7',
+    price: 850,
+    description: ''
+  },
+  {
+    name: 'Samsung S8',
+    price: 900,
+    description: ''
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
