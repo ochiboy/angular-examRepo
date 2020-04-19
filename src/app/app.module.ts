a38cddff5b604cd1f0d6bf480b632705cc041a3a
ochiboy <chieshing@gmail.com>
2020-04-19 23:11:46 +0800

    1 file modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'; 
 
@@ -14,10 +12,7 @@ import { SearchBarComponent } from './search-bar/search-bar.component';
   imports: [
     HttpClientModule,
     BrowserModule,
-    ReactiveFormsModule,
     FormsModule,
-    RouterModule.forRoot([
-    ])
   ],
   declarations: [
     AppComponent,
